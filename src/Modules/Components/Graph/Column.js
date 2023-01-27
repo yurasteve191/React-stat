2cc321ea279ee5aa7a6b4e9b93265aa94d0d7e93
Daniel Kurapov <zarogon@gmail.com>
2023-01-27 20:10:06 +0200

    Update Column.js

--- src/Modules/Components/Graph/Column.js
+++ src/Modules/Components/Graph/Column.js
@@ -2,20 +2,19 @@ function Column({ column,filterColors }) {
   const { top } = column
     console.log(filterColors);
   //
-  let categories = []
-  Object.keys(column).forEach((element) => {
-    if (element !== "top") {
-      const elStyle = {
-        flexGrow: column[element].toString(),
-        // backgroundColor: filterColors[element]
-      }
-      categories.push(
-        <div style={elStyle} className={`column-category ${filterColors[element]}`}>
-          {column[element]}
-        </div>
-      )
-    }
-  })
+  let categories = Object.keys(column).reduce(
+		(result, item) =>
+			item !== top ? [
+				...result,
+				<div
+					style={{ flexGrow: column[item].toString() }}
+					className={`column-category ${filterColors[item]}`}
+				>
+					{column[item]}
+				</div>,
+			] : result,
+		[]
+	);
   //
   const heightCol = 400 - top + "px"
   const mainStyle = {
